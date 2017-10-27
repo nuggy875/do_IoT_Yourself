d45fbf8ec33458f73e3d8e9da7f08f090cbfba1a
seunkyu2 <zx6658@naver.com>
2017-10-27 23:44:07 +0900

    하드웨어에서 json 데이터 받아와서, 서버에서 처리후 json 배열로 이벤트 보내주거나, mail 보내기 등 자체 서버처리 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var gravatar = require('gravatar');
 var Connects = require('../models/connects');
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +12,10 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/home', hasAuthorization, function(req, res) {
+
     // List all connects and sort by Date
     Connects.find().sort('-created').populate('user', 'local.email').exec(function(error, connects) {
+
         if (error) {
             return res.send(400, {
                 message: error
@@ -26,11 +29,64 @@ router.get('/home', hasAuthorization, function(req, res) {
         });
     });
 });
+/*
+hardware에서 데이터를 받아 그에맞는 event처리
+*/
+router.post('/data_status', function(req, res) {
+   //post방식으로 받아온 json타입에서 바디의 iotaction key의 data값을 string으로 받아옴
+   // iotaction: xx
+   var iot_action=req.body.iotaction
+   console.log(iot_action);
+
+  //json 배열 형태로 뽑아내 전달
+
+  Connects.find({iotaction: iot_action},{_id: 0, iotevent: 1})
+  .select('iotevent').sort('-created')
+  .exec(function(error, connects) {
+    var iot_events = (connects);
+    var idx = iot_events.find((item, idx) => {
+      return item.iotevent === 'E-mail 전송'
+    });
 
 
+    //응답이 email 제외한경우 json으로 이벤트전달
+     if(idx == undefined){
+      console.log(iot_events);
+      res.json(iot_events);
+    }
+
+    else{
+      var transporter = nodemailer.createTransport({
+      service: "naver",
+      auth: {
+              user: "zx6658@naver.com",
+              pass: "as960920!!"
+        }
+      });
+
+      var mailOptions = {
+        from: "zx6658@naver.com",
+        to: "zx6658@hanmail.net",
+        subject: "iot_test ",
+        text: "Hello "
+        };
+
+      transporter.sendMail(mailOptions, function(error, response)
+      {
+        if(error){ console.log(error);
+        }
+        else{
+          console.log("Message sent: " + response.message);
+        }
+        transporter.close();
+      });
+    }
+  });
+});
 
 /* Create connects */
 router.post('/home', function(req, res) {
+  console.log(req.body);
     // create a new instance of the Connects model with request body
     var connects = new Connects(req.body);
     // Set current user (id)
