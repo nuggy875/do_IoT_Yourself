a64831ed6185179af895be2c1eaee535660196c3
Gilbert Paeng <jinugpaeng@gmail.com>
2017-10-27 23:59:48 +0900

    pulling

--- server/routes/index.js
+++ server/routes/index.js
@@ -104,7 +104,7 @@ function isLoggedIn(req, res, next) {
 /* GET Logout Page */
 router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect('/');
+    res.redirect('/home');
 });
 
 
