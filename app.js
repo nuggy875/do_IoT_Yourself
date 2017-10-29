5df5b019e357df4b3cd245e3737420d27ac5964c
Gilbert Paeng <jinugpaeng@gmail.com>
2017-10-30 03:00:50 +0900

    db fix

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.set('view engine', 'ejs');
 // Database configuration
 var config = require('./server/config/config.js');
 // connect to our database
-mongoose.connect(config.url);
+mongoose.connect(config.url ,{
+	useMongoClient:true
+});
 // Check if MongoDB is running
 mongoose.connection.on('error', function() {
 	console.error('MongoDB 연결오류, MongoDB를 연결해주세요.');
